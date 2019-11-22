a2f0fff21c993b77eb592b1e7bc80d40cd319981
bas <bas@willemstijn.net>
2019-11-22 22:40:30 +0100

    Version 0.9 works

--- BitcoinLottery.py
+++ BitcoinLottery.py
@@ -3,42 +3,60 @@
 # Generate bitcoin keys and address
 import requests
 from bitcoin import *
+import time
+import sqlite3
 
-def create_addr():
-	priv = random_key()
-	pub = privtopub(priv)
-	addr = pubtoaddr(pub)
-        electrumPKey = encode_privkey(priv, 'wif')
-	return addr, priv, electrumPKey
+# Start timer section
+def executeScript():
+    
+	# Generating keys
+	def create_addr():
+		priv = random_key()
+		pub = privtopub(priv)
+		addr = pubtoaddr(pub)
+		electrumPKey = encode_privkey(priv, 'wif')
+		return addr, priv, electrumPKey
 
-addr,priv,electrumPKey = create_addr()
+	addr,priv,electrumPKey = create_addr()
 
-# Print keys to console (for checking code)
-print("Electrum Import Key: " + electrumPKey)
-print("Private Key: " + priv)
-print("BTC Address: " + addr)
+	# Print keys to console (for checking code)
+	print("Electrum Import Key: " + electrumPKey)
+	print("Private Key: " + priv)
+	print("BTC Address: " + addr)
 
-# Get the amount of BTC on the address
-req = requests.get('https://blockchain.info/balance?active='+(addr))
+	# Get the amount of BTC on the address
+	req = requests.get('https://blockchain.info/balance?active='+(addr))
 
-# Print output to the screen for checking code
-#print req.text
+	# Print output to the screen for checking code
+	#print req.text
 
-# Extracting the BTC amount from blockchain.info output
-splitter1=req.text.split(":")[2]
-splitter2=splitter1.split(",")[0]
+	# Extracting the BTC amount from blockchain.info output
+	splitter1=req.text.split(":")[2]
+	splitter2=splitter1.split(",")[0]
 
-# Send amount to console for checking code
-#print(splitter2)
+	# Send amount to console for checking code
+	#print(splitter2)
 
-# Convert satoshi to BTC
-SatoshiConvert = float(splitter2) / 100000000.0
-print ("Amount on address: " + str(SatoshiConvert))
+	# Convert satoshi to BTC
+	SatoshiConvert = float(splitter2) / 100000000.0
+	print ("Amount on address: " + str(SatoshiConvert))
+	BitcoinConvert = str(SatoshiConvert)
+	
+	# Writing output to comma separated file
+	# text_file = open("BTC.csv", "a+")
+	# text_file.write(BitcoinConvert + "," + addr + "," + electrumPKey + "," + priv)
+	# text_file.write("\n")
+	# text_file.close()
 
-# Writing output to comma separated file
-text_file = open("BTC.csv", "a+")
-text_file.write(str(SatoshiConvert) + "," + addr + "," + electrumPKey + "," + priv)
-text_file.write("\n")
-text_file.close()
+	# Put output in SQLite database
+	conn = sqlite3.connect("lottery.db")
+	c = conn.cursor()
+	c.execute("INSERT INTO addresses (amount,btc_address,electrum_key,private_key) VALUES (?, ?, ?, ?)",(BitcoinConvert,addr,electrumPKey,priv))
+	conn.commit()
+	conn.close()
 
-# ================= ADDITIONAL TEST CODE HERE ====================
+# Timer section part 2
+time.sleep(4)
+
+while True:
+    executeScript()
--- README.md
+++ README.md
@@ -7,4 +7,48 @@ The following packages have to be installed to make the lottery work:
 * sudo apt install python-pip
 * pip install blockstack
 * pip install bitcoin
+* sudo apt install sqlite3
+* sudo apt install sqlitebrowser
+* apt-get install screen
 
+## Create a SQLite database for storing found keys
+Create the database with the following commands
+
+```bash
+# Create new database in the directory
+sqlite3 lottery.db
+
+# Create table in the database
+CREATE TABLE addresses ( 
+amount INT NOT NULL, 
+btc_address VARCHAR(50), 
+electrum_key VARCHAR(70), 
+private_key VARCHAR(80), 
+PRIMARY KEY (btc_address) );
+
+# Show the tables with
+.tables
+
+# Show the table schema with
+.schema addresses
+
+# Quit sqlite
+.quit
+```
+
+## Running the script in the background
+For now I use this quick and dirty method of runing the script in the background.
+
+```bash
+# Create a new virtual terminal
+screen -dmS btcl
+
+# Connect to the virtual screen
+screen -r btcl
+
+# Run the script
+./BitcoinLottery.py
+```
+
+Close your terminal screen. The BitcoinLottery PID belongs to the virtual terminal and keeps running.  
+To check the script, run ''screen -r btcl'' again
